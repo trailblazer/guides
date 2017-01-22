125d6c856ae281989b712f9cbacaf6e2f26da5d4
Nick Sutterer <apotonick@gmail.com>
2017-01-22 21:10:56 +1100

    more to test.

--- app/concepts/blog_post/operation/create.rb
+++ app/concepts/blog_post/operation/create.rb
@@ -8,8 +8,10 @@ module Procedural
   class BlogPost::Create < Trailblazer::Operation
     step :do_everything!
 
+    #:args
     def do_everything!(options, params:, current_user:, **)
       return unless current_user.signed_in?
+    #:args end
 
       model = BlogPost.new
       model.update_attributes(params[:blog_post])
@@ -24,3 +26,59 @@ module Procedural
   #:procedural end
 end
 
+module ProceduralSet
+  BlogPost = ::BlogPost
+
+  class BlogPost::Create < Trailblazer::Operation
+    step :do_everything!
+
+    #:procedural-set
+    def do_everything!(options, params:, current_user:, **)
+      return unless current_user.signed_in?
+
+      model = BlogPost.new
+      options["model"] = model # readable in steps and result.
+
+      model.update_attributes(params[:blog_post])
+      #~hide
+      if model.save
+        BlogPost::Notification.(current_user, model)
+      else
+        return false
+      end
+      #~hide end
+    end
+    #:procedural-set end
+  end
+end
+
+module FirstSteps
+  BlogPost = ::BlogPost
+
+  #:firststeps
+  class BlogPost::Create < Trailblazer::Operation
+    step :authorize!
+    step :model!
+    step :persist!
+    step :notify!
+
+    def authorize!(options, current_user:, **)
+      current_user.signed_in?
+    end
+
+    def model!(options, **)
+      options["model"] = BlogPost.new
+    end
+
+    def persist!(options, params:, model:, **)
+      model.update_attributes(params[:blog_post])
+      model.save
+    end
+
+    def notify!(options, current_user:, model:, **)
+      BlogPost::Notification.(current_user, model)
+    end
+  end
+  #:firststeps end
+
+end
--- spec/concepts/blog_post/operation/create_spec.rb
+++ spec/concepts/blog_post/operation/create_spec.rb
@@ -19,11 +19,53 @@ module Procedural
   #:procedural end
 
     it "works with known user" do
+      #:dependencies
       result = BlogPost::Create.(pass_params, "current_user" => signed_in)
+      #:dependencies end
 
       expect(result).to be_success
       expect(BlogPost.last).to be_persisted
       expect(BlogPost.last.title).to eq("Puns: Ode to Joy")
+    end
+  end
+end
+
+module ProceduralSet
+   RSpec.describe BlogPost::Create do
+    let (:anonymous) { User.new(false) }
+    let (:signed_in) { User.new(true) }
+    let (:pass_params) { { blog_post: { title: "Puns: Ode to Joy" } } }
+
+    #:set-spec
+    it "fails with anonymous" do
+      result = BlogPost::Create.(pass_params, "current_user" => anonymous)
+
+      expect(result).to be_failure
+      expect(result["model"]).to be_nil
+    end
+    #:set-spec end
+  end
+end
+
+module FirstSteps
+  RSpec.describe BlogPost::Create do
+    let (:anonymous) { User.new(false) }
+    let (:signed_in) { User.new(true) }
+    let (:pass_params) { { blog_post: { title: "Puns: Ode to Joy" } } }
+
+    it "fails with anonymous" do
+      result = BlogPost::Create.(pass_params, "current_user" => anonymous)
+
+      expect(result).to be_failure
+      expect(result["model"]).to be_nil
+    end
+
+    it "works with known user" do
+      result = BlogPost::Create.(pass_params, "current_user" => signed_in)
+
+      expect(result).to be_success
+      expect(result["model"]).to be_persisted
+      expect(result["model"].title).to eq("Puns: Ode to Joy")
     end
   end
 end
