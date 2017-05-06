83df3ac922aba1487f4f6c2756ab08ba1a3b5218
Nick Sutterer <apotonick@gmail.com>
2017-05-06 13:45:52 +0200

    fix routing and Rack.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resource :blog_posts
+  resources :blog_posts
 end
