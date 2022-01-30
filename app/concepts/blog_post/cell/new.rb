0e1cd34aa12df720e7a92b6268167d744834bdc9
Nick Sutterer <apotonick@gmail.com>
2022-01-30 16:57:14 +0100

    for now, remove cells.

--- app/concepts/blog_post/cell/new.rb
+++
@@ -1,9 +0,0 @@
-#:cell
-module BlogPost::Cell
-  class New < Trailblazer::Cell
-    include ActionView::RecordIdentifier
-    include ActionView::Helpers::FormOptionsHelper
-    include SimpleForm::ActionViewExtensions::FormHelper
-  end
-end
-#:cell end
